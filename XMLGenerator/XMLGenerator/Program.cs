4922aa217bd4cb02f723b0563c40a86761c31c0c
WhiteKoldun <50768001+WhiteKoldun@users.noreply.github.com>
2021-03-04 17:49:50 +0300

    Initial commit

---
+++ XMLGenerator/XMLGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XMLGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
